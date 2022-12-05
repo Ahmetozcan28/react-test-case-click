3bd432c492142c30fbf7ae49e2caac89e876bddf
Ahmet <mrkronoss@hotmail.com>
2022-12-05 14:47:12 +0300

    merge

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 function App() {
   const [points, setPoints] = useState([]);
-
+  const [data , setData] = useState([]);
+  console.log(points);
   const clickHandle = (e) => {
     setPoints((points) => [
       ...points,
@@ -11,13 +12,35 @@ function App() {
         y: e.clientY,
       },
     ]);
+    setData([])
   };
+  const redoHandle = (e) => {
+    e.stopPropagation()
+    const data = [...points]
+    const item = data.pop()
+    setData(data => [...data, item])
+    setPoints(data)
+  };
+  const undoHandle = (e) => {
+    e.stopPropagation()
+    const d = [...data]
+    const item = d.pop()
+    setPoints(points => [...points, item])
+    setData(d)
+
+  }
   return (
-    <div className="screen" onClick={clickHandle}>
-      {points.map((point) => (
-        <div className="point" style={{ left: point.x, top: point.y }} />
-      ))}
-    </div>
+    <>
+      <div className="screen" onClick={clickHandle}>
+        <header className="header">
+          <button disabled={points.length === 0} onClick={redoHandle}>Geri Al</button>
+          <button disabled={data.length === 0} onClick={undoHandle} >İleri Al</button>
+        </header>
+        {points.map((point) => (
+          <div className="point" style={{ left: point.x, top: point.y }} />
+        ))}
+      </div>
+    </>
   );
 }
 
