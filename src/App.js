e524a8b6e6568a5179c58a20df3344b80dc51e5a
Ahmet <mrkronoss@hotmail.com>
2022-12-05 13:40:23 +0300

    merge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-
+import { useState } from "react";
 
 function App() {
-  return <div className="screen">Test</div>;
+  const [points, setPoints] = useState([]);
+
+  const clickHandle = (e) => {
+    setPoints((points) => [
+      ...points,
+      {
+        x: e.clientX,
+        y: e.clientY,
+      },
+    ]);
+  };
+  return (
+    <div className="screen" onClick={clickHandle}>
+      {points.map((point) => (
+        <div className="point" style={{ left: point.x, top: point.y }} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
