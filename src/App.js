9f2d32e513123ef2cd40f7ba4be0ada4f66ab742
Ahmet <mrkronoss@hotmail.com>
2022-12-05 18:30:46 +0300

    fix: merge

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App() {
   const [points, setPoints] = useState([]);
-  const [data , setData] = useState([]);
+  const [data, setData] = useState([]);
   const clickHandle = (e) => {
     setPoints((points) => [
       ...points,
@@ -11,32 +11,40 @@ function App() {
         y: e.clientY,
       },
     ]);
-    setData([])
+    setData([]);
   };
   const undoHandle = (e) => {
-    e.stopPropagation()
-    const data = [...points]
-    const item = data.pop()
-    setData(data => [...data, item])
-    setPoints(data)
+    e.stopPropagation();
+    const data = [...points];
+    const item = data.pop();
+    setData((data) => [...data, item]);
+    setPoints(data);
   };
   const redoHandle = (e) => {
-    e.stopPropagation()
-    const d = [...data]
-    const item = d.pop()
-    setPoints(points => [...points, item])
-    setData(d)
-
-  }
+    e.stopPropagation();
+    const d = [...data];
+    const item = d.pop();
+    setPoints((points) => [...points, item]);
+    setData(d);
+  };
   return (
     <>
       <div className="screen" onClick={clickHandle}>
         <header className="header">
-          <button disabled={points.length === 0} onClick={undoHandle}>Geri Al</button>
-          <button disabled={data.length === 0} onClick={redoHandle} >İleri Al</button>
+          <button disabled={points.length === 0} onClick={undoHandle}>
+            Geri Al
+          </button>
+          <button disabled={data.length === 0} onClick={redoHandle}>
+            İleri Al
+          </button>
         </header>
         {points.map((point) => (
-          <div className="point" style={{ left: point.x, top: point.y }} />
+          <div
+            className=" point material-symbols-outlined"
+            style={{ color: "yellow",   left: point.x, top: point.y }}
+          >
+            electric_bolt
+          </div>
         ))}
       </div>
     </>
